0d8b4cae351608baeea0139e6ffdebeb9468fb46
Juan Lucha <juanlucha@gmail.com>
2019-10-08 18:47:14 +0200

    Added any kind of dice throws

--- extension.js
+++ extension.js
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
+const times = require('lodash').times
+
+const options = {
+  oraculo: '1. oráculo',
+  tirada: '2. tirada'
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,7 +14,7 @@ const vscode = require('vscode')
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+function activate (context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "zerogm" is now active!')
@@ -16,11 +22,11 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.zeroGM', function() {
+  let disposable = vscode.commands.registerCommand('extension.zeroGM', function () {
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
-    vscode.window.showQuickPick(['1. tirada', '2. generador']).then(manageMainAction)
+    vscode.window.showQuickPick([options.oraculo, options.tirada]).then(manageMainAction)
   })
 
   context.subscriptions.push(disposable)
@@ -28,48 +34,125 @@ function activate(context) {
 exports.activate = activate
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate () {}
 
 module.exports = {
   activate,
-  deactivate
-}
+deactivate}
 
-/////////////////////
+// ///////////////////
 // Logic functions //
-/////////////////////
+// ///////////////////
 
-function manageMainAction(selection) {
-  if (selection === '1. tirada') {
-    vscode.window
-      .showInputBox({
-        placeHolder: '¿Cómo de probable? (-4 a +4 si es una pregunta al master)',
-        validateInput: value => {
-          if (isNaN(value)) {
-            return `El valor debe ser un número`
-          }
-        }
-      })
-      .then((modifier) => {
-				if (modifier === '') modifier = 0
-        const fateCheck = fateDiceCheck(parseInt(modifier))
-        writeToDocument(fateCheck)
-      })
+function manageMainAction (selection) {
+  if (selection === options.oraculo) {
+    doOracleCheck()
+  } else {
+    doDiceCheck()
   }
 }
 
-function fateDiceCheck(modifier) {
+function doOracleCheck () {
+  vscode.window
+    .showInputBox({
+      placeHolder: '¿Cómo de probable? (-4 a +4 si es una pregunta al master)',
+      validateInput: value => {
+        if (isNaN(value)) {
+          return `El valor debe ser un número`
+        }
+      }
+    })
+    .then((modifier) => {
+      if (modifier === '') modifier = 0
+      const fateCheck = fateDiceCheck(parseInt(modifier))
+      writeToDocument(fateCheck)
+    })
+}
+
+function doDiceCheck () {
+  vscode.window
+    .showInputBox({
+      placeHolder: '¿Qué dados lanzar? (20, 100, 1d20, 1d8, f)',
+      validateInput: value => {
+        if (!value) {
+          return `Debes introducir un valor para los dados`
+        } else if (isNaN(value) && !value.includes('d')) {
+          return `El valor de los dados debe ser un número o en formato xdy (1d4, 2d6, etc...)`
+        }
+      }
+    })
+    .then((dice) => {
+      return Promise.all([
+        Promise.resolve(dice),
+        vscode.window
+          .showInputBox({
+            placeHolder: '¿Cuál es el motivo de la tirada?'
+          })
+      ])
+    })
+    .then(([dice, message = '']) => {
+      const diceCheck = checkDice(dice)
+      if (!dice.includes('d')) {
+        dice = `1d${dice}`
+      }
+      const completeCheck = `\`\`\`
+${message}
+${dice} -> ${diceCheck}
+\`\`\`
+
+`
+      writeToDocument(completeCheck)
+    })
+}
+
+function checkDice (dice) {
+  let numberOfDices = 1
+  let kindOfDice
+  if (dice.toLowerCase().includes('d')) {
+    const parsedDice = dice.toLowerCase().split('d')
+    numberOfDices = parsedDice[0]
+    kindOfDice = parsedDice[1]
+  } else {
+    kindOfDice = dice
+  }
+  let result = []
+  times(numberOfDices, () => {
+    const check = Math.floor(Math.random() * kindOfDice + 1)
+    result.push(check)
+  })
+  return formatDiceCheck(result)
+}
+
+function formatDiceCheck (results) {
+  let resultCheck = ''
+  let total = 0
+
+  results.forEach((result, index) => {
+    resultCheck += `${result}`
+    total += parseInt(result)
+    if (index < results.length - 1) {
+      resultCheck += ` + `
+    } else {
+      resultCheck += ` = `
+    }
+  })
+  resultCheck += ` ${total}`
+
+  return resultCheck
+}
+
+function fateDiceCheck (modifier) {
   let fateCheck = [0, 0, 0, 0]
   fateCheck = fateCheck.map(() => fateDice())
   const fateCheckValue = fateCheck.reduce((acumulator, dice) => acumulator + dice) + modifier
   return formatFateCheck(fateCheck, modifier, fateCheckValue)
 }
 
-function fateDice() {
+function fateDice () {
   return Math.floor(Math.random() * 3 + 2) - 3
 }
 
-function writeToDocument(value) {
+function writeToDocument (value) {
   const editor = vscode.window.activeTextEditor
   const position = editor.selection.active
   editor.edit(edit => {
@@ -77,7 +160,7 @@ function writeToDocument(value) {
   })
 }
 
-function formatFateCheck(fateCheck, modifier, fateCheckValue) {
+function formatFateCheck (fateCheck, modifier, fateCheckValue) {
   let formattedCheck = fateCheck.reduce((accumulator, value, index) => {
     switch (value) {
       case -1:
@@ -102,10 +185,10 @@ ${formattedCheck} + ${modifier} = ${fateCheckValue}
 ${resultInText}
 \`\`\`
 		
-		`
+`
 }
 
-function checkFateCheckAnswer(value) {
+function checkFateCheckAnswer (value) {
   if (value === 8) {
     return `Definitivamente sí`
   } else if (value >= 0) {
@@ -117,7 +200,7 @@ function checkFateCheckAnswer(value) {
   }
 }
 
-function checkSideOutcome(fateCheck) {
+function checkSideOutcome (fateCheck) {
   let sideOutcome = ''
   let positives = 0
   let negatives = 0
@@ -152,7 +235,7 @@ const outcomeTargets = [
   `tu personaje`
 ]
 
-function getOutcomeTarget() {
+function getOutcomeTarget () {
   const index = Math.floor(Math.random() * 10) + 1
 
   return outcomeTargets[index]
